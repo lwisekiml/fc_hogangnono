6665cc39e6617f3e1ebb824c2ada9b49a553814a
wisekim <lwisekiml@naver.com>
2024-02-03 18:41:29 +0900

    Ch 01. 실거래가 시스템 요구사항 분석 - 01. 요구사항 분석하기

---
+++ src/test/java/com/fc/housebatch/HouseBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.housebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
