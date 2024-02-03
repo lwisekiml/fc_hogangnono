b7d78e419be4b0b065eb51663582791294676223
wisekim <lwisekiml@naver.com>
2024-02-03 23:35:11 +0900

    Ch 03. Batch 프로그램 개발 - 03. 동 배치 - 설계와 엔티티 구현

---
+++ src/main/java/com/fc/housebatch/core/repository/LawdRepository.java
@@ -0,0 +1,8 @@
+package com.fc.housebatch.core.repository;
+
+import com.fc.housebatch.core.entity.Lawd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LawdRepository extends JpaRepository<Lawd, Long> {
+
+}
